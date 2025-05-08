813d64eadfe1fa52e5b8007332f5947e20fa4abb
Lin <1160983879@qq.com>
2025-05-08 17:44:33 +0800

    1

--- hello/src/Main.java
+++ hello/src/Main.java
@@ -1,11 +1,1 @@
-/**
- * @Author: Lin
- * @Description: TODO
- * @Version: v1.0.0
- * @Date: 2025/2/8 下午 9:09
- **/
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("11111!");
-    }
-}
+2
